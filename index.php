ff58bbce1ad7f2aebe970f57070df5982d4ebd36
Chris <christopher.zig0803@gmail.com>
2021-04-13 11:14:04 +0200

    design global

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cinémas à salles multiples du D5P</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="navbar">
+		<a href="">1ère requête</a>
+		<a href="">2ème requête</a>
+		<a href="">3ème requête</a>
+		<a href="">4ème requête</a>
+		<a href="">5ème requête</a>
+	</div>
+
+	<h2>Récupérer la liste des films qui seront projetés à l'endroit, au jour et à l'heure que vous désirez</h2>
+	<div class="requete1">
+		<form>
+			<label>Nom de l'entreprise :</label><br>
+			<input type="text" name="entreprise"><br>
+			<label>Nom de la ville :</label><br>
+			<input type="text" name="ville"><br>
+			<label>Nom du cinéma :</label><br>
+			<input type="text" name="cinema"><br>
+			<label>Nom du film :</label><br>
+			<input type="text" name="film"><br>
+			<label>Nom de l'acteur :</label><br>
+			<input type="text" name="acteur"><br>
+			<label>Date :</label>
+			<input type="date" name="date"><br>
+			<label>Heure :</label><br>
+			<input type="time" name="heure"><br>
+			<br>
+			<a href="">Rechercher</a>
+		</form>
+	</div>
+</body>
+</html>
