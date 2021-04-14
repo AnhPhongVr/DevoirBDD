d5bb93b7d29bd5529a30b73a58348a85817fe953
Chris <christopher.zig0803@gmail.com>
2021-04-14 12:17:42 +0200

    modification de la page principale et ajout d'une partie du code php

---
+++ Requete1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Requête1</title>
+</head>
+<body>
+	<p>Bonjour à tous, nous allons répondre à <b>la première requête</b> sur cette page.</p>
+	<p>La liste des films qui seront projetés à <b><?php echo $_POST['ville']; ?></b>, le <b><?php echo $_POST['date']; ?></b> à <b><?php echo $_POST['heure']; ?></b> est :</p>
+	<p>Pour revenir à la page d'accueil, <a href="index.php">cliquez ici</a>.</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<div class="navbar">
-		<a href="">1ère requête</a>
+		<a href="index.php">1ère requête</a>
 		<a href="">2ème requête</a>
 		<a href="">3ème requête</a>
 		<a href="">4ème requête</a>
@@ -15,23 +15,15 @@
 
 	<h2>Récupérer la liste des films qui seront projetés à l'endroit, au jour et à l'heure que vous désirez</h2>
 	<div class="requete1">
-		<form>
-			<label>Nom de l'entreprise :</label><br>
-			<input type="text" name="entreprise"><br>
+		<form method="POST" action="Requete1.php">
 			<label>Nom de la ville :</label><br>
 			<input type="text" name="ville"><br>
-			<label>Nom du cinéma :</label><br>
-			<input type="text" name="cinema"><br>
-			<label>Nom du film :</label><br>
-			<input type="text" name="film"><br>
-			<label>Nom de l'acteur :</label><br>
-			<input type="text" name="acteur"><br>
 			<label>Date :</label>
 			<input type="date" name="date"><br>
 			<label>Heure :</label><br>
 			<input type="time" name="heure"><br>
 			<br>
-			<a href="">Rechercher</a>
+			<button type="submit">Rechercher</button>
 		</form>
 	</div>
 </body>
