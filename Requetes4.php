829092d710ebea739cb7a7e1e656983577b9ad69
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-04-15 14:49:40 +0200

    req4 fini sans etre fini, y'a pas assez de donnée

--- Requetes4.php
+++ Requetes4.php
@@ -5,8 +5,8 @@
 	<title>Requetes4</title>
 </head>
 <body>
-	<p>Bonjour à tous, nous allons répondre à <b> la quatrième requete</b>sur cette page.</p>
-	<p>La liste des entreprises qui ont projeté plus de 10 films pendant chaque mois de l'année <b><?php echo $_POST['date']; ?></b> à <b><?php echo $_POST['ville'];?></b>est:</p>
+	<p>Bonjour à tous, nous allons répondre à <b> la quatrième requete </b>sur cette page.</p>
+	<p>La liste des entreprises qui ont projeté plus de 3 films pendant chaque mois de l'année <b><?php echo $_POST['date']; ?></b> à <b><?php echo $_POST['lieu'];?></b> est:</p>
 	<?php
 
 	try
@@ -24,18 +24,18 @@
 	                JOIN salle ON salle.idCinema = cinéma.idCinema 
                     JOIN projection ON projection.idSalle = salle.idSalle 
                     JOIN film ON film.idFilm = projection.idFilm
-	WHERE cinéma.ville = :ville AND YEAR(projection.date) = YEAR(:annee) AND 
+	WHERE cinéma.ville = :ville AND YEAR(projection.date) = YEAR(:annee) AND
 	(SELECT COUNT(projection.idProjection) 
     FROM entreprise JOIN cinéma ON entreprise.idEntreprise = cinéma.idEntreprise 
                     JOIN salle ON salle.idCinema = cinéma.idCinema
                     JOIN film ON film.idFilm = projection.idFilm
     WHERE YEAR(projection.date) = YEAR(:annee)
-    GROUP BY MONTH(projection.date)) > 10");
-	$req->execute(array('ville' => $_POST['ville'], 'annee' => $_POST['date']))
+    GROUP BY MONTH(projection.date)) > 3");
+	$req->execute(array('ville' => $_POST['lieu'], 'annee' => $_POST['date']));
 
     while ($donnees = $req->fetch())
     {
-        echo 'il y a ' . $donnees['titre'] . ' le '. $donnees['date'] . ' à ' . $donnees['horaire'] . ' à ' . $donnees['ville'] . '<br />';
+        echo 'il y a ' . $donnees['nom'] . '<br />';
     }
 
     $req->closeCursor();
